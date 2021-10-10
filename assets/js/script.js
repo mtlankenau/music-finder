22cd6cef40b0a5eaf7ad7f1864cd8ec77509ebda
mtlankenau <mtlankenau@gmail.com>
2021-10-09 23:34:21 -0400

    windows 11 CORS issue test

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,76 +2,63 @@
 const searchInputEl = document.querySelector("#artist-name");
 const searchButtonEl = document.querySelector("#search-button");
 const albumCheckboxEl = document.querySelector("#album");
+const concertsCheckboxEl = document.querySelector("#concerts");
 
 // event listener for search button click
 searchButtonEl.addEventListener("click", musicTermFinder);
 
 // function run after clicking the search button
 function musicTermFinder(event) {
-    // text entered into input is now 'searchedMusicTerm' variable
-    var searchedMusicTerm = searchInputEl.value.toLowerCase();
-    console.log(searchedMusicTerm);
+    // if 'I want to see their albums' checkbox is checked,
     if (albumCheckboxEl.checked === true) {
-        var albumEntity = "&entity=album";
+        albumApiFunction();
+    } else if (concertsCheckboxEl.checked === true) {
+        concertsApiFunction();
     } else {
-        albumEntity = "";
+        showAlert();
     }
+};
 
-    // on button click, fetch API for searched music term
+function albumApiFunction() {
+    // text entered into input is now 'searchedMusicTerm' variable, converted to lowercase
+    var searchedMusicTerm = searchInputEl.value.toLowerCase();
+    // sets album entity query parameter as a variable
+    var albumEntity = "&entity=album";
+    // fetch API for searched music term
     fetch("https://itunes.apple.com/search?term=" + searchedMusicTerm + albumEntity + "&attribute=artistTerm&limit=10")
         .then(function(response) {
-        return response.json();
+            return response.json();
         })
         .then(function(data) {
             console.log(data);
-            // if album checkbox is checked, return an object with relevant data to all 10 albums
-            if (albumCheckboxEl.checked === true) {
-                // loops through each album, index 0 - 9
-                for (var i = 0; i < data.results.length; i++) {
-                    console.log(searchedMusicTerm + data.results[i].artistName.toLowerCase());
-                    if (searchedMusicTerm.includes(data.results[i].artistName.toLowerCase())) {
-                        // object with album name, genre, release date, and url
-                        var albumDataObject = {
-                            albumNames: data.results[i].collectionName,
-                            albumGenre: data.results[i].primaryGenreName,
-                            albumReleaseDate: data.results[i].releaseDate,
-                            albumUrl: data.results[i].collectionViewUrl,
-                            albumArt: data.results[i].artworkUrl60
-                        }
-                        // logs object with information
-                        console.log(albumDataObject)
+            // loops through each album, index 0 - 9
+            for (var i = 0; i < data.results.length; i++) {
+                console.log(searchedMusicTerm + data.results[i].artistName.toLowerCase());
+                if (searchedMusicTerm.includes(data.results[i].artistName.toLowerCase())) {
+                    // object with album name, genre, release date, and url
+                    var albumDataObject = {
+                        albumNames: data.results[i].collectionName,
+                        albumGenre: data.results[i].primaryGenreName,
+                        albumReleaseDate: data.results[i].releaseDate,
+                        albumUrl: data.results[i].collectionViewUrl,
+                        albumArt: data.results[i].artworkUrl60
                     }
-                };
-            // if album checkbox isn't checked, return an object with information related to the artist
-            } else {
-                showAlert();
-            }
-        });
-    // prevents browser from refreshing after clicking on search button
-    event.preventDefault();
+                    // logs object with information
+                    console.log(albumDataObject)
+                }
+            };
+        })
 };
 
-function showAlert(){
-    var notificationDivEl = document.querySelector("#notification");
-    var notificationAlert = document.createElement("p");
-    notificationAlert.textContent = "Please select at least 1 checkbox before clicking the 'Search' button";
-    notificationDivEl.appendChild(notificationAlert);
-};
+function concertsApiFunction() {
+    // text entered into input is now 'searchedMusicTerm' variable, converted to lowercase
+    var searchedMusicTerm = searchInputEl.value.toLowerCase();
 
-
-fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=drake&apikey=ff5u94LXWXFZZinXI0G9v1Y5GICPDiG5")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
-
-
-// fetch("http://www.songsterr.com/a/ra/songs/byartists.json?artists=Metallica")
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(data){
-//         console.log(data);
-//     })
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchedMusicTerm + "&apikey=ff5u94LXWXFZZinXI0G9v1Y5GICPDiG5")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+}
