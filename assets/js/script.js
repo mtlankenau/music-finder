0032054027db7a8950f2d259ddf3a4c62930428a
brendanjlewis <brendan.j.f.lewis@gmail.com>
2021-10-13 20:31:41 -0400

    added card function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,13 @@ const concertsIconEl = document.querySelector("#concerts-icon");
 const favoriteAlbumButtonEl = document.querySelector("#favorite-album");
 const favoriteAlbumIconEl = document.querySelector("#favorite-album-icon");
 const albumCardsContainerEl = document.querySelector("#album-cards-container");
+const venueCardsContainterEl = document.querySelector("#venue-cards-container");
 
 // event listener for search button click
 albumSelectorEl.addEventListener("click", albumIconAdjuster);
 concertsSelectorEl.addEventListener("click", concertsIconAdjuster);
 searchButtonEl.addEventListener("click", musicTermFinder);
-favoriteAlbumIconEl.addEventListener("click", selectFavoriteAlbum);
+//favoriteAlbumIconEl.addEventListener("click", selectFavoriteAlbum);
 
 function albumIconAdjuster() {
     if (albumIconEl.textContent === "add") {
@@ -118,23 +119,23 @@ function concertsApiFunction() {
     	return response.json();
     })
     .then(function(data) {
-      console.log(data);
+      	console.log(data);
 			for (var i = 0; i < data._embedded.events.length; i++) {
 				if (searchedMusicTerm.includes(data._embedded.events[i].name.toLowerCase()) && data._embedded.events[i].dates.status.code == "onsale") {
 				// object with album name, genre, release date, and url
-				var venueDataObject = {
+					var venueDataObject = {
 							venueName: data._embedded.events[i]._embedded.venues[0].name,
 							artistName: data._embedded.events[i].name,
 							venueAddress: data._embedded.events[i]._embedded.venues[0].address.line1 + ", " + data._embedded.events[i]._embedded.venues[0].city.name + ", " + data._embedded.events[i]._embedded.venues[0].state.stateCode + " " + data._embedded.events[i]._embedded.venues[0].postalCode,
 							eventStartDate: data._embedded.events[i].dates.start.localDate + " at " + data._embedded.events[i].dates.start.localTime,
 							ticketAvailability: data._embedded.events[i].dates.status.code,
-							buyTicketsUrl: data._embedded.events[i].url
+							buyTicketsUrl: data._embedded.events[i].url,
+					}
+					// logs object with information
+					console.log(venueDataObject);
+					createVenueCards(venueDataObject);
 				}
-				// logs object with information
-				console.log(venueDataObject);
-				// createVenueCards(venueDataObject);
-				}
-			}	
+			};	
     })
 };
 
@@ -219,6 +220,55 @@ function createAlbumCards(albumDataObject) {
 
 	albumCardDivEl.appendChild(albumCardEl);
 	albumCardsContainerEl.appendChild(albumCardDivEl);
+};
+
+
+
+function createVenueCards(venueDataObject) {
+	//elements
+	var venueCardDivEl = document.createElement("div");
+	var venueCardEl = document.createElement("div");
+	var venueCardImageDivEl = document.createElement("div");
+	var venueCardImageEl = document.createElement("img");
+	var venueCardContentEl = document.createElement("div");
+	var venueCardContentSpanEl = document.createElement("span");
+	var venueCardContentSpanIEl = document.createElement("i");
+	var venueCardSpanEl = document.createElement("span");
+	var venueCardSpanIEl = document.createElement("i");
+	var venueCardContentParagraphAnchorEl = document.createElement("a");
+	var venueCardContentParagraphEl = document.createElement("p");
+
+	//css attributes
+	venueCardDivEl.setAttribute("class", "col s3 hoverable");
+	venueCardEl.setAttribute("class", "card");
+	venueCardImageDivEl.setAttribute("class", "card-image waves-effect waves-block waves-light");
+	venueCardImageEl.setAttribute("class", "activator");
+	//venueCardImageEl.setAttribute("src", venueDataObject.venueImage.replace("100x100bb", "1000x1000bb"));
+	venueCardImageEl.setAttribute("height", "250vh");
+	venueCardContentEl.setAttribute("class", "card-content");
+	venueCardContentSpanEl.setAttribute("class", "card-title activator grey-text text-darken-4");
+	venueCardContentSpanIEl.setAttribute("class", "material-icons right");
+	venueCardContentParagraphAnchorEl.setAttribute("href", venueDataObject.buyTicketsUrl);
+	venueCardContentParagraphAnchorEl.setAttribute("target", "_blank");
+
+	//text content
+	venueCardContentSpanEl.textContent = venueDataObject.venueName;
+	venueCardContentSpanIEl.textContent = "more_vert";
+	venueCardContentParagraphAnchorEl.textContent = "Buy tickets";
+
+	//append elements
+	venueCardImageDivEl.appendChild(venueCardImageEl);
+
+	venueCardContentSpanEl.appendChild(venueCardContentSpanIEl);
+	venueCardContentParagraphEl.appendChild(venueCardContentParagraphAnchorEl);
+	venueCardContentEl.appendChild(venueCardContentSpanEl);
+	venueCardContentEl.appendChild(venueCardContentParagraphEl);
+	
+	venueCardEl.appendChild(venueCardImageDivEl);
+	venueCardEl.appendChild(venueCardContentEl);
+
+	venueCardDivEl.appendChild(venueCardEl);
+	venueCardsContainterEl.appendChild(venueCardDivEl);
 };
 
 function showAlert() {
